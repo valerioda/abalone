fffc47e168de2729b9805f23f06f601ce0a84c96
valerioda <valerio.dandrea@lngs.infn.it>
2021-03-30 17:37:57 +0200

    simulation analysis

--- calculate_integrals.py
+++ calculate_integrals.py
@@ -13,6 +13,11 @@ import scipy.integrate as integ
 from scipy.optimize import curve_fit
 import time
 
+from sklearn.externals.joblib import Parallel, delayed
+import multiprocessing
+num_cores = multiprocessing.cpu_count()
+
+
 def main():
     
     par = argparse.ArgumentParser(description = 'SiPM integral calculate')
@@ -24,6 +29,8 @@ def main():
     arg('-v', '--voltages', nargs=2, action='store', help = 'Voltage interval')
     arg('-f', '--file', nargs=1, action='store', help = 'Data file')
     arg('-ft', '--fitparameters', nargs=5, action='store', help = 'Fit parameters')
+    arg('-st', '--searchparameters', nargs=2, action='store', help = 'Search parameters')
+    arg('-p', '--parallelize', action=st, help = 'Parallelize integrals')
     args = vars(par.parse_args())
 
     if args['date']:
@@ -37,10 +44,13 @@ def main():
     else:
         print('Number of SiPM not given')
         return
-
+    
     parameters = [-4, 1, 10, 100, 4]
     if args['fitparameters']:
         parameters = np.array(args['fitparameters'][:],dtype=int)
+    searchparameters = [4, 2]
+    if args['searchparameters']:
+        searchparameters = np.array(args['searchparameters'][:],dtype=int)
     
     npeaks = 0
     if args['number']: npeaks = int(args['number'][0])
@@ -68,7 +78,7 @@ def main():
             try: data = psu.read_file(f'/data/abalone/{date}/SiPM{sipm}_{v_int}_{v_frac}_LED2p75.dat')
             except: return
             print('SiPM',sipm,'Voltage =',v_int+v_frac/10,'V, Total events:',len(data))
-            peakint = calculate_integrals( data, sipm, parameters, npeaks, d_out )
+            peakint = calculate_integrals( data, sipm, searchparameters, parameters, npeaks, d_out )
             np.save(f'{d_out}/peakint_SiPM{sipm}_{v_int}_{v_frac}_LED2p75_{date}.npy', peakint)
             return
 
@@ -87,49 +97,51 @@ def main():
         return
     print('Calculate integrals for ',datafile,'Total events:',len(data))
     dataname = datafile.split('/')[-1].split('.')[0]
-    peakint = calculate_integrals( data, sipm, parameters, npeaks, d_out )
+    if args['parallelize']:
+        peakint = calculate_integrals_par( data, sipm, searchparameters, parameters, npeaks, d_out )
+    else:
+        peakint = calculate_integrals( data, sipm, searchparameters, parameters, npeaks, d_out )
     np.save(f'{d_out}/peakint_{dataname}.npy', peakint)
     return
     
     
-def calculate_integrals( data, sipm, par, npeaks = 0, d_out = '.' ):
+def calculate_integrals( data, sipm, spar, par, npeaks = 0, d_out = '.' ):
+    print('Use of standard loop')
     nn = len(data)
     peakint = np.zeros(nn)
     t_start = time.time()
+    print('Search parameters:',spar)
     print('Fit parameters:',par)
     if npeaks == 0: npeaks = nn
     for i in range(npeaks):
-        listpeaks = psu.search_peaks(data[i], 4, 2, False)
-        peakint[i] = psu.integral_central_peak(data[i],listpeaks,par[0],par[1],par[2],par[3],par[4],8,10)
+        listpeaks = psu.search_peaks(data[i], spar[0], spar[1], False)
+        peakint[i] = psu.integral_central_peak(data[i],listpeaks,par[0],par[1],par[2],par[3],par[4],8,10,central=False)
         diff = time.time() - t_start
         if (i % 1000) == 0:
             print(f'event n. {i} area: {peakint[i]:.2f}, time to process: {diff:.2f}')
+    print(f'total time to process: {diff:.2f}')
     print()
     return peakint
 
 
-from joblib import Parallel, delayed
-import multiprocessing
-num_cores = multiprocessing.cpu_count()
-
-
-def parrallel_integrals(wf,par)
-    listpeaks = psu.search_peaks(wf, 4, 2, False)
-    peakint = psu.integral_central_peak(wf,listpeaks,par[0],par[1],par[2],par[3],par[4],8,10)
-    
+def parrallel_integrals(wf,spar,par):
+    listpeaks = psu.search_peaks(wf, spar[0], spar[1], False)
+    peakint = psu.integral_central_peak(wf,listpeaks,par[0],par[1],par[2],par[3],par[4],8,10,central=False)
     return peakint
 
-def calculate_integrals_par( data, sipm, par, npeaks = 0, d_out = '.' ):
+def calculate_integrals_par( data, sipm, spar, par, npeaks = 0, d_out = '.' ):
+    print('Use of parallelized loop')
     nn = len(data)
     peakint = np.zeros(nn)
     t_start = time.time()
+    print('Search parameters:',spar)
     print('Fit parameters:',par)
     if npeaks == 0: npeaks = nn
-    peakint = Parallel(n_jobs=num_cores)(delayed(parrallel_integrals)(data[i],par) for i in range(npeaks)  )
+    peakint = Parallel(n_jobs=num_cores)(delayed(parrallel_integrals)(data[i],spar,par) for i in range(npeaks)  )
     peakint = np.array(peakint)
     
     diff = time.time() - t_start
-    print(f' time to process: {diff:.2f}')
+    print(f'Total time to process: {diff:.2f}')
     return peakint
 
 if __name__=="__main__":
